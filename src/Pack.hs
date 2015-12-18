e0f7cd84572df337ca18771a7d0b5772b33c87d4
kinoru <xkinoru@gmail.com>
2015-12-18 08:51:25 +0000

    Add Pack module
    This module serves as a replacement of `show` for efficiency.

---
+++ src/Pack.hs
@@ -0,0 +1,15 @@
+module Pack
+    ( str
+    , int
+    ) where
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.ByteString.Builder as B
+
+str :: String -> ByteString
+str = LB.toStrict . B.toLazyByteString . B.stringUtf8
+
+int :: Int -> ByteString
+int = LB.toStrict . B.toLazyByteString . B.intDec
