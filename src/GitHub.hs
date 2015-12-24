23f08b68666b361267b5ac2fb04c47a96869fb9f
kinoru <xkinoru@gmail.com>
2015-12-24 11:31:22 +0000

    GitHub API: add createRelease

--- src/GitHub.hs
+++ src/GitHub.hs
@@ -3,10 +3,21 @@ module GitHub where
 import Control.Lens
 import Network.Wreq
 
-import qualified Data.ByteString as SB
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString.Lazy as LB
 
-basic :: SB.ByteString -> IO (Response LB.ByteString)
-basic token = getWith opts "https://api.github.com/"
+createRelease
+    :: ByteString
+    -> ByteString
+    -> ByteString
+    -> ByteString
+    -> IO (Response LB.ByteString)
+createRelease token owner repo tagName = postWith opts uri content
   where
-    opts = defaults & header "Authorization" .~ [token]
+    opts = defaults & header "Authorization" .~ ["token " `mappend` token]
+    uri = C.unpack $ mconcat
+        ["https://api.github.com/repos/", owner, "/", repo, "/releases"]
+    content = mconcat
+        [ "{\"tag_name\": \"", tagName, "\",\"name\":\"", tagName
+        , "\",\"draft\":true}"]
