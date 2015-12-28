238690aabff0fe91aa2dacef642773618b5c9091
kinoru <xkinoru@gmail.com>
2015-12-28 14:15:57 +0000

    Add configuration file format module

---
+++ src/Format.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Format
+    ( FromJSON
+    , dec
+    , EncodeJSON(enc)
+    , Conf(..)
+    , initConf
+    ) where
+
+import Data.Text
+import Data.ByteString (ByteString)
+import Data.ByteString.Lazy (fromStrict, toStrict)
+
+import GHC.Generics (Generic)
+import Data.Aeson
+import Data.Aeson.Encode.Pretty
+
+dec :: FromJSON a => ByteString -> Either String a
+dec = eitherDecode . fromStrict
+
+encBase :: ToJSON a => [Text] -> a -> ByteString
+encBase keys = (toStrict .) $ encodePretty' $ Config 4 $ keyOrder keys
+
+-- a separate class for explicit ordering of JSON fields
+class EncodeJSON a where
+    enc :: a -> ByteString
+
+data Conf = Conf
+    { owner :: !Text
+    , repo :: !Text
+    }
+    deriving (Show, Generic)
+
+instance FromJSON Conf
+instance ToJSON Conf
+instance EncodeJSON Conf where
+    enc = encBase ["owner", "repo"]
+
+initConf :: Conf
+initConf = Conf
+    "owner"
+    "repo"
